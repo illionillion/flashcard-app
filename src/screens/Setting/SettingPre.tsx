123e115263d84ccbf1a329dcf76e6cc8ee17b574
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-05-23 19:58:10 +0900

    設定画面のUI作成

--- src/screens/Setting/SettingPre.tsx
+++ src/screens/Setting/SettingPre.tsx
@@ -1,13 +1,29 @@
-import { FC } from "react";
-import { Text, View } from "react-native";
+import { FC, useState } from "react";
+import { Text, View, TextInput, Linking, TouchableOpacity } from "react-native";
 
 /**
  * 設定画面のUI
  */
 export const SettingPre:FC = () => {
+  const [searchText, setSearchText] = useState("");
+
+  const handleButtonPress = () => {
+    const url = '';
+    Linking.openURL(url);
+  }
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>設定画面</Text>
+    <View style={{ flex: 1, flexDirection:'column', justifyContent: 'flex-start', alignItems: 'center', marginTop: 15 }}>
+      {/* <Text>設定画面</Text> */}
+      <TextInput
+        style={{textAlign: "center",height: 40, width: 250, borderWidth: 1, borderColor:"grey", marginBottom: 10, borderRadius:5 }}
+        placeholder="APIキー設定を入力"
+      />
+      <View style={{alignSelf: "flex-end", marginRight: 20}}>
+        <TouchableOpacity onPress={handleButtonPress} style={{ padding: 10}}>
+          <Text style={{color: "#a9ceec", textDecorationLine: "underline", textAlign: "right" }}>APIキーの取得方法</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
