1292e1b6ea3497373a0c5174fc355b7cb76215b1
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-05-23 22:03:36 +0900

    PRレビューを元に修正

--- src/screens/Setting/SettingPre.tsx
+++ src/screens/Setting/SettingPre.tsx
@@ -1,5 +1,12 @@
 import { FC, useState } from "react";
-import { Text, View, TextInput, Linking, TouchableOpacity } from "react-native";
+import { 
+  Text, 
+  View, 
+  TextInput, 
+  Linking, 
+  TouchableOpacity,
+  StyleSheet,
+ } from "react-native";
 
 /**
  * 設定画面のUI
@@ -11,19 +18,46 @@ export const SettingPre:FC = () => {
     const url = '';
     Linking.openURL(url);
   }
-
+  const styles = StyleSheet.create({
+    allView: {
+      flex: 1,
+      flexDirection:'column',
+      justifyContent: 'flex-start', 
+      alignItems: 'center', 
+      marginTop: 15
+    },
+    input:{
+      height: 40, 
+      width: 250, 
+      borderWidth: 1, 
+      borderColor:"grey", 
+      marginBottom: 10, 
+      borderRadius:5
+    },
+    textView: {
+      alignSelf: "flex-end", 
+      marginRight: 20
+    },
+    text: {
+      color: "#a9ceec", 
+      textDecorationLine: "underline", 
+      textAlign: "right"
+    }
+  })
+  
   return (
-    <View style={{ flex: 1, flexDirection:'column', justifyContent: 'flex-start', alignItems: 'center', marginTop: 15 }}>
+    <View style={styles.allView}>
       {/* <Text>設定画面</Text> */}
       <TextInput
-        style={{textAlign: "center",height: 40, width: 250, borderWidth: 1, borderColor:"grey", marginBottom: 10, borderRadius:5 }}
+        style={styles.input}
         placeholder="APIキー設定を入力"
       />
-      <View style={{alignSelf: "flex-end", marginRight: 20}}>
+      <View style={styles.textView}>
         <TouchableOpacity onPress={handleButtonPress} style={{ padding: 10}}>
-          <Text style={{color: "#a9ceec", textDecorationLine: "underline", textAlign: "right" }}>APIキーの取得方法</Text>
+          <Text style={styles.text}>APIキーの取得方法</Text>
         </TouchableOpacity>
       </View>
     </View>
   );
+
 };
