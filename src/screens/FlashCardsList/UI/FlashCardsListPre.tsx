63dee20143f6fb3c4afafd11f59e5cba99c62ba8
yuto ichinose <yuto.0526.sano@gmail.com>
2023-05-26 22:32:52 +0900

    単語帳がない場合の表示 #5

--- src/screens/FlashCardsList/UI/FlashCardsListPre.tsx
+++ src/screens/FlashCardsList/UI/FlashCardsListPre.tsx
@@ -1,22 +1,30 @@
 import { FC } from "react";
-import { StyleSheet, View, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import { RenderItem } from "../Components/RenderItem";
 import { FlashCardsDef } from "../../../atom/FlashCardsDataState";
 
 interface FlashCardsListPreProps {
-  onPressCard: (id: number) => void;
   rows: FlashCardsDef[][];
+  onPressCard: (id: number) => void;
+  onPressButton: () => void
 }
 
 /**
  * 単語帳一覧のUI
  */
 export const FlashCardsListPre: FC<FlashCardsListPreProps> = ({
-  onPressCard,
   rows,
+  onPressCard,
+  onPressButton
 }) => {
-  return (
-    <ScrollView contentContainerStyle={styles.container}>
+  return rows.length > 0 ? (
+    <ScrollView contentContainerStyle={styles.ScrollContainer}>
       {rows.map((row, index) => (
         <View key={index} style={styles.row}>
           {row.map((item) => (
@@ -34,11 +42,39 @@ export const FlashCardsListPre: FC<FlashCardsListPreProps> = ({
         </View>
       ))}
     </ScrollView>
+  ) : (
+    <View style={styles.container}>
+      <Text style={styles.heading}>単語帳はありません</Text>
+      <TouchableOpacity style={styles.button} onPress={onPressButton}>
+        <Text style={styles.buttonText}>単語帳を作成する</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 15,
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: "700",
+  },
+  button: {
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderRadius: 5,
+    backgroundColor: "#79BC6E",
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: "700",
+    color: "#fff",
+  },
+  ScrollContainer: {
     flexGrow: 1,
     paddingHorizontal: 8,
   },
