35dcff13cad0aef2e3b9386a1d122445e7cb11e5
takeki <takekidanmkk@gmail.com>
2023-05-27 01:22:51 +0900

    三角ボタンを押すと, ページ数を変更できるようにした.

--- src/screens/Slide/UI/SlidePre.tsx
+++ src/screens/Slide/UI/SlidePre.tsx
@@ -1,11 +1,26 @@
-import { FC } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { FC, useState } from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import { SlideButton } from "./Components/SlideButton";
 
 interface Props {
 }
 
 export const SlidePre: FC<Props> = (props) => {
+  const [page, setPage] = useState(1);
+  const pageTotal = 8;
+
+  const handlePageChange = (page: number) => {
+    if (page < 1) {
+      setPage(1);
+    } else if (page > pageTotal) {
+      setPage(pageTotal);
+    } else {
+      setPage(page);
+    }
+  };
+
+
+
   return (
     <View style={styles.container}>
 
@@ -14,17 +29,21 @@ export const SlidePre: FC<Props> = (props) => {
       </View>
 
       <View style={styles.slide}>
-        <Image source={require("./images/triangle_button_left.png")} />
+        <TouchableOpacity onPress={() => handlePageChange(page - 1)}>
+          <Image source={require("./images/triangle_button_left.png")} />
+        </TouchableOpacity>
         <View style={styles.content}>
           <Text style={styles.content_text}>
             コンテンツ
           </Text>
         </View>
-        <Image source={require("./images/triangle_button_right.png")} />
+        <TouchableOpacity onPress={() => handlePageChange(page + 1)}>
+          <Image source={require("./images/triangle_button_right.png")} />
+        </TouchableOpacity>
       </View>
 
       <View style={styles.pagenation}>
-        <Text>2/8</Text>
+        <Text>{page}/8</Text>
       </View>
 
       <SlideButton text="切り替え" />
