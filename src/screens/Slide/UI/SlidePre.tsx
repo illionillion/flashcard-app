452b815bfc56751453b6a7c815dda5161ff11987
takeki <takekidanmkk@gmail.com>
2023-05-26 22:02:59 +0900

    要素を並べた

--- src/screens/Slide/UI/SlidePre.tsx
+++ src/screens/Slide/UI/SlidePre.tsx
@@ -1,10 +1,51 @@
 import { FC } from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { SlideButton } from "./Components/SlideButton";
 
 export const SlidePre: FC = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>スライド</Text>
+    <View style={styles.container}>
+
+      <View style={styles.headline}>
+        <Text>見出し</Text>
+      </View>
+
+      <View style={styles.slide}>
+        <Image source={require("./images/triangle_button_left.png")} />
+        <View>
+          <Text>Slide</Text>
+        </View>
+        <Image source={require("./images/triangle_button_right.png")} />
+      </View>
+
+      <View style={styles.pagenation}>
+        <Text>2/8</Text>
+      </View>
+
+      <SlideButton text="切り替え" />
+      <SlideButton text="終了" />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  headline: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
+  slide: {
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    flexDirection: "row",
+  },
+  pagenation: {
+    justifyContent: "center",
+  },
+});    
