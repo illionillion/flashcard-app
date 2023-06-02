78383fb1f556ff3be46578d1a9d13ac4e9ab77af
unreact-hatano <mizutama.hahaha@gmail.com>
2023-06-02 20:03:28 +0900

    単語検索画面の挙動修正
    「該当する単語が見つかりません」の追加
    dataの更新に伴いfilteredDataを更新する

--- src/screens/Search/UI/SearchCon.tsx
+++ src/screens/Search/UI/SearchCon.tsx
@@ -62,7 +62,7 @@ export const SearchCon: FC<SearchConProps> = ({ navigation }) => {
     });
   };
 
-  // 画面遷移するとsearchValueをリセット
+  // 画面遷移に伴いsearchValueをリセットする
   useEffect(() => {
     const unsubscribe = navigation.addListener("blur", () => {
       setSearchValue("");
@@ -70,6 +70,19 @@ export const SearchCon: FC<SearchConProps> = ({ navigation }) => {
 
     return unsubscribe;
   }, [navigation]);
+
+  // dataの更新に伴いfilteredDataを更新する
+  useEffect(() => {
+    const newConvertedData = data.flatMap((cards) =>
+      cards.words.map((word) => ({
+        ...word,
+        fileId: cards.id,
+        fileName: cards.name,
+        isOpen: false,
+      })),
+    );
+    setFilteredData(newConvertedData);
+  }, [data]);
 
   return (
     <SearchPre
