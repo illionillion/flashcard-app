abf9cca27bdc611028b17a6e43642b300361ab86
unreact-hatano <mizutama.hahaha@gmail.com>
2023-06-06 18:21:36 +0900

    UI修正

--- src/screens/Setting/UI/SettingPre.tsx
+++ src/screens/Setting/UI/SettingPre.tsx
@@ -24,7 +24,6 @@ export const SettingPre: FC<SettingPreProps> = ({
   return (
     <View style={styles.allView}>
       <View style={styles.row}>
-        <Text style={styles.label}>APIキー : </Text>
         {isEditMode ? (
           <TextInput
             style={styles.input}
@@ -34,17 +33,20 @@ export const SettingPre: FC<SettingPreProps> = ({
             keyboardType="visible-password"
           />
         ) : (
-          <Text style={styles.apiKey}>
-            {apiKey.length > 7
-              ? `${apiKey.substring(0, 3)}...${apiKey.substring(apiKey.length - 4)}`
-              : apiKey || "未設定"}
-          </Text>
+          <>
+            <Text style={styles.label}>APIキー : </Text>
+            <Text style={styles.apiKey}>
+              {apiKey.length > 7
+                ? `${apiKey.substring(0, 3)}...${apiKey.substring(apiKey.length - 4)}`
+                : apiKey || "未設定"}
+            </Text>
+          </>
         )}
       </View>
+      <TouchableOpacity onPress={handleClickToggleEditModeButton} style={styles.button}>
+        <Text style={styles.buttonText}>{isEditMode ? "保存する" : "APIキーを編集する"}</Text>
+      </TouchableOpacity>
       <View style={styles.textView}>
-        <TouchableOpacity onPress={handleClickToggleEditModeButton}>
-          <Text style={styles.editButton}>{isEditMode ? "保存する" : "編集する"}</Text>
-        </TouchableOpacity>
         <TouchableOpacity
           onPress={() => handleLinkPress("https://ray-boon-api.vercel.app/how-to-setting")}
         >
@@ -66,30 +68,40 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "flex-start",
     alignItems: "center",
-    marginTop: 15,
+    marginTop: 30,
   },
   row: {
     flexDirection: "row",
-    width: "85%",
+    width: "80%",
     alignItems: "center",
     height: 40,
   },
-  label: { fontSize: 16, textAlign: "left" },
   input: {
     height: 40,
-    width: "70%",
     paddingHorizontal: 16,
     borderWidth: 1,
     borderColor: "grey",
     borderRadius: 5,
     flex: 1,
   },
+  label: { fontSize: 16 },
   apiKey: { fontSize: 16, flex: 1, marginLeft: 4, color: "gray" },
   textView: {
     alignSelf: "flex-end",
     marginRight: 20,
   },
-  editButton: { textAlign: "right", padding: 10, color: "gray" },
+  button: {
+    backgroundColor: "#5FA1DE",
+    height: 40,
+    width: "80%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+    marginVertical: 30,
+  },
+  buttonText: {
+    color: "white",
+  },
   link: {
     padding: 10,
     color: "#06C3FF",
