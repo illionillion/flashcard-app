c7b46ff1463be1691c7ea58d82de72529dea0b46
unreact-hatano <mizutama.hahaha@gmail.com>
2023-07-04 15:01:35 +0900

    例文読み上げ機能の実装  #64
    expo-speechの導入・実装

--- src/screens/Slide/UI/SlideCon.tsx
+++ src/screens/Slide/UI/SlideCon.tsx
@@ -1,41 +1,56 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NavigationProp, RouteProp } from '@react-navigation/native';
+import * as Speech from 'expo-speech';
 import { FC, useState } from 'react';
-import { SlidePre } from './SlidePre';
-import { NavigationProp, RouteProp, useNavigation } from '@react-navigation/native';
 import StackParamList from '../../../StackParamList';
+import { SlidePre } from './SlidePre';
 
 type FlashCardsViewRouteProp = RouteProp<StackParamList, 'Slide'>;
 interface SlideConProps {
   navigation: NavigationProp<any, any>;
   route: FlashCardsViewRouteProp;
- }
+}
 
-export const SlideCon: FC<SlideConProps> = ({navigation, route}) => {
-	const { data } = route.params;
-	const handleGoBack = () => {
-		navigation.goBack();
-	};
+export const SlideCon: FC<SlideConProps> = ({ navigation, route }) => {
+  const { data } = route.params;
+  const [page, setPage] = useState(0);
+  const [isFront, setIsFront] = useState(true);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const pageTotal = data.length;
 
-	const [page, setPage] = useState(0);
-	const pageTotal = data.length;
-	const handlePageChange = (page: number) => {
-		if (page >= 0 && page <= pageTotal - 1) {
-			setPage(page);
-		}
-	};
+  const handleGoBack = () => {
+    navigation.goBack();
+  };
 
-	const [isFront, setIsFront] = useState(true);
-	const handleFlip = () => {
-		setIsFront(!isFront);
-	};
+  const handlePageChange = (page: number) => {
+    if (page >= 0 && page <= pageTotal - 1) {
+      setPage(page);
+    }
+  };
 
-	return (
-		<SlidePre
-			handleGoBack={handleGoBack}
-			word_list={data}
-			page={page}
-			handlePageChange={handlePageChange}
-			isFront={isFront}
-			handleFlip={handleFlip}
-		/>
-	);
+  const handleFlip = () => {
+    setIsFront(!isFront);
+  };
+
+  const handlePressSpeaker = (example: string) => {
+    Speech.speak(example, {
+      rate: 1.0,
+      pitch: 1.0,
+      onStart: () => setIsSpeaking(true),
+      onDone: () => setIsSpeaking(false),
+    });
+  };
+
+  return (
+    <SlidePre
+      handleGoBack={handleGoBack}
+      word_list={data}
+      page={page}
+      handlePageChange={handlePageChange}
+      isFront={isFront}
+      handleFlip={handleFlip}
+      isSpeaking={isSpeaking}
+      handlePressSpeaker={handlePressSpeaker}
+    />
+  );
 };
