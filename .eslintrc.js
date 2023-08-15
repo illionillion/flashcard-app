74ee12e7cca2818820b4d0d5874e74982e0a4b67
yuto ichinose <yuto.0526.sano@gmail.com>
2023-08-15 23:27:00 +0900

    fix: ルール修正 #99 - no-mixed-spaces-and-tabsを無効化 - import typeを強制 - propsのソート

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,52 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true,
-	},
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:react/recommended',
-	],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: ['@typescript-eslint', 'react'],
-	rules: {
-		'react/jsx-uses-react': 'off',
-		'react/react-in-jsx-scope': 'off',
-		'linebreak-style': ['error', 'unix'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'always'],
-	},
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+	// no-mixed-spaces-and-tabsルールを削除するか、offに設定する
+    'no-mixed-spaces-and-tabs': 'off',
+    // // インデントはスペースで統一
+    // indent: ['error', 2, { SwitchCase: 1 }],
+
+    // // タブ文字は使用しない
+    // 'no-tabs': 'error',
+
+    /**
+     * @description importが型のみの場合は、import typeを強制
+     */
+    '@typescript-eslint/consistent-type-imports': 'error',
+
+    /**
+     * @description propsを自動でソート
+     * @see {@link https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-sort-props.md}
+     */
+    'react/jsx-sort-props': [
+      'error',
+      {
+        callbacksLast: true,
+        shorthandFirst: true,
+        ignoreCase: true,
+        noSortAlphabetically: true,
+        reservedFirst: true,
+      },
+    ],
+  },
 };
